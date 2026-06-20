50c09a4baa24dc42e0142abcc7e6f3f03424df44
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2026-06-20 12:09:32 -0300

    Primeiro commit

---
+++ src/main/java/com/arenapay/arenapay/ArenapayApplication.java
@@ -0,0 +1,13 @@
+package com.arenapay.arenapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenapayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenapayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arenapay/arenapay/ArenapayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arenapay.arenapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenapayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
