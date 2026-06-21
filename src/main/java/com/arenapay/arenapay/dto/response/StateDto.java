2a769c9a79fb243102ddef2fc365adeb8aa1bf2c
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2026-06-21 02:23:14 -0300

    fix(state): mudando nome de classe dto.

--- src/main/java/com/arenapay/arenapay/dto/response/StateDto.java
+++
@@ -1,9 +0,0 @@
-package com.arenapay.arenapay.dto.response;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-import java.util.UUID;
-
-public record StateDto(@Schema(example = "b4279c1c-a4ec-4ae7-827a-59de10c3ea1c", description = "State id") UUID id,
-                       @Schema(example = "Pará", description = "State name") String name) {
-}
---
+++ src/main/java/com/arenapay/arenapay/dto/response/StateResponseDto.java
@@ -0,0 +1,9 @@
+package com.arenapay.arenapay.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.UUID;
+
+public record StateResponseDto(@Schema(example = "b4279c1c-a4ec-4ae7-827a-59de10c3ea1c", description = "State id") UUID id,
+                               @Schema(example = "Pará", description = "State name") String name) {
+}
