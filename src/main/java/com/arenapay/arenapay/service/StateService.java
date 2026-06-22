97c18ac6808cfb04568f6652b7ccde3d1d3013c2
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2026-06-22 17:44:42 -0300

    fix(state): versão final do metodo findAll.

--- src/main/java/com/arenapay/arenapay/service/StateService.java
+++ src/main/java/com/arenapay/arenapay/service/StateService.java
@@ -1,20 +1,43 @@
 package com.arenapay.arenapay.service;
 
-import com.arenapay.arenapay.model.State;
+import com.arenapay.arenapay.dto.response.StateResponseDto;
+import com.arenapay.arenapay.mapper.StateMapper;
 import com.arenapay.arenapay.repository.StateRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 @Service
 public class StateService {
     private final StateRepository stateRepository;
+    private final StateMapper stateMapper;
+    private static final Logger log = LoggerFactory.getLogger(StateService.class);
 
-    public StateService(StateRepository stateRepository) {
+    public StateService(StateRepository stateRepository, StateMapper stateMapper) {
         this.stateRepository = stateRepository;
+        this.stateMapper = stateMapper;
     }
 
-    public List<State> findAll() {
-        return stateRepository.findAll();
+    @Transactional
+    public List<StateResponseDto> findAll() {
+        log.info("Iniciando a busca de estados no banco de dados.");
+
+        Instant start = Instant.now();
+
+        List<StateResponseDto> responseList = stateRepository.findAll()
+                .stream().map(stateMapper::toResponse).toList();
+
+        long timeTaken = Duration.between(start, Instant.now()).toMillis();
+        if (timeTaken > 2000) {
+            log.warn("ALERTA DE PERFORMANCE: A listagem demorou {}ms para responder!", timeTaken);
+        } else {
+            log.info("Busca finalizada. Tempo gasto: {}ms para encontrar {} estados no banco.", timeTaken, responseList.size());
+        }
+        return responseList;
     }
 }
