23ef7bc202e1486932a6b8da8632c5203fe86ca7
= <devhenriquerodrigues971@gmail.com>
2026-06-22 23:40:02 -0300

    fix(state): criar metodo de busca por nome

--- src/main/java/com/arenapay/arenapay/repository/StateRepository.java
+++ src/main/java/com/arenapay/arenapay/repository/StateRepository.java
@@ -3,8 +3,9 @@ package com.arenapay.arenapay.repository;
 import com.arenapay.arenapay.model.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface StateRepository extends JpaRepository<State, UUID> {
-
+    Optional<State> findByNameIgnoreCase(String name);
 }
