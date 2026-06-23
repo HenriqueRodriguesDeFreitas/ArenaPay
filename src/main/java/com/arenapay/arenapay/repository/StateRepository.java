6377641d9ba3d555993027598f3d6a4fb2b2f5e2
= <devhenriquerodrigues971@gmail.com>
2026-06-23 00:26:36 -0300

    fix(state): add query ao metodo de busca por nome.

--- src/main/java/com/arenapay/arenapay/repository/StateRepository.java
+++ src/main/java/com/arenapay/arenapay/repository/StateRepository.java
@@ -2,10 +2,13 @@ package com.arenapay.arenapay.repository;
 
 import com.arenapay.arenapay.model.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface StateRepository extends JpaRepository<State, UUID> {
-    Optional<State> findByNameIgnoreCase(String name);
+    @Query("SELECT s FROM State s WHERE function('unaccent', lower(s.name)) = function('unaccent', lower(:name)) ")
+    Optional<State> findByNameIgnoreCase(@Param("name") String name);
 }
