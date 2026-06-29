1421bb7557e00a13c195ab6bc6e8c15ec13f114f
= <devhenriquerodrigues971@gmail.com>
2026-06-29 20:56:12 -0300

    fix(city): add example no response.

--- src/main/java/com/arenapay/arenapay/dto/response/CityResponse.java
+++ src/main/java/com/arenapay/arenapay/dto/response/CityResponse.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.UUID;
 
-public record CityResponse(@Schema(name = "id", example = "City id") UUID id,
-                           @Schema(name = "name", description = "City name") String name,
-                           @Schema(name = "state", example = "State name") String state) {
+public record CityResponse(@Schema(name = "id", example = "b4279c1c-a4ec-4ae7-827a-59de10c3ea1c") UUID id,
+                           @Schema(name = "name", example = "Breves") String name,
+                           @Schema(name = "state", example = "Pará") String state) {
 }
