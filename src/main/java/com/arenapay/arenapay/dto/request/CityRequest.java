4b0833b3de1b728f368694442c99d065b6b5913d
= <devhenriquerodrigues971@gmail.com>
2026-06-29 20:55:58 -0300

    fix(city): add validações  no record de requisição.

--- src/main/java/com/arenapay/arenapay/dto/request/CityRequest.java
+++ src/main/java/com/arenapay/arenapay/dto/request/CityRequest.java
@@ -1,6 +1,8 @@
 package com.arenapay.arenapay.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
-public record CityRequest(@Schema(name = "Breves", examples = "City name") String name, @Schema(name = "Pará", example = "State name") String state) {
+public record CityRequest(@Schema(name = "name", examples = "Breves") @NotBlank String name,
+                          @Schema(name = "state", example = "Pará") @NotBlank String state) {
 }
