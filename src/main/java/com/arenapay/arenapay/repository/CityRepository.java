80863e4ebf07809de1589970b7fb06ac95e344c9
= <devhenriquerodrigues971@gmail.com>
2026-06-29 18:26:58 -0300

    fix(city): criar repositorio retorna boolean.

--- src/main/java/com/arenapay/arenapay/repository/CityRepository.java
+++ src/main/java/com/arenapay/arenapay/repository/CityRepository.java
@@ -2,8 +2,11 @@ package com.arenapay.arenapay.repository;
 
 import com.arenapay.arenapay.model.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface CityRepository extends JpaRepository<City, UUID> {
+    @Query("SELECT COUNT(c) > 0 FROM City c WHERE function('unaccent', lower(c.name)) = function('unaccent', lower(:name)) AND c.state.id = :stateId")
+    boolean findByNameIgnoreCaseAndStateById(String name, UUID id);
 }
